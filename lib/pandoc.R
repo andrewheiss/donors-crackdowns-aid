029d3fbafb56068c4f949aa6767ce4ec841f9ba1
Andrew Heiss <andrew@andrewheiss.com>
2021-02-25 01:36:35 -0500

    Initial commit

---
+++ lib/pandoc.R
@@ -0,0 +1,5 @@
+panderOptions('table.split.table', Inf)
+panderOptions('table.split.cells', Inf)
+panderOptions('keep.line.breaks', TRUE)
+panderOptions('table.style', 'multiline')
+panderOptions('table.alignment.default', 'left')
