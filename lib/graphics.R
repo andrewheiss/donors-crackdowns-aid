04a18bf3198bd29c44d787ca8c399e5aa0abd7a6
Andrew Heiss <andrew@andrewheiss.com>
2021-03-14 15:02:36 -0400

    Add DAGs, general analysis, and MSMs for H1 and H2

--- lib/graphics.R
+++ lib/graphics.R
@@ -1,7 +1,7 @@
 update_geom_defaults("bar", list(fill = "grey30"))
 update_geom_defaults("line", list(colour = "grey30"))
-update_geom_defaults("label", list(family = "Work Sans Light", face = "plain"))
-update_geom_defaults("text", list(family = "Work Sans Light", face = "plain"))
+update_geom_defaults("label", list(family = "Noto Sans", face = "plain"))
+update_geom_defaults("text", list(family = "Noto Sans", face = "plain"))
 # update_geom_defaults("label_repel", list(family = "Work Sans", face = "plain))
 # update_geom_defaults("text_repel", list(family = "Work Sans", face = "plain))
 
@@ -55,15 +55,35 @@ fig_save_cairo <- function(fig, filepath = here("analysis", "output"),
          width = width, height = height, units = units, type = "cairo", dpi = 300, ...)
 }
 
-theme_donors <- function(base_size = 9, base_family = "Work Sans Light") {
+#' Convert mms to pts
+#' 
+#' Convert units specified in millimeters to typographic points. This is especially helpful when working with ggplot geoms that use size parameters
+#'
+#' @param x a numeric value (in millimeters)
+#'
+#' @return A numeric value (in points)
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' 
+#' ggplot(mtcars, aes(x = mpg, y = wt)) +
+#'   geom_point() +
+#'   annotate(geom = "text", x = 20, y = 4, 
+#'            label = "Here's a label", size = pts(11))
+pts <- function(x) {
+  as.numeric(grid::convertUnit(grid::unit(x, "pt"), "mm"))
+}
+
+theme_donors <- function(base_size = 11, base_family = "Noto Sans", prior = FALSE) {
   ret <- theme_bw(base_size, base_family) +
     theme(panel.background = element_rect(fill = "#ffffff", colour = NA),
           title = element_text(size = rel(1.1), vjust = 1.2, 
-                               family = "Work Sans Medium", face = "plain"),
+                               family = "Noto Sans", face = "bold"),
           plot.subtitle = element_text(size = rel(0.8), 
-                                       family = "Work Sans Light", face = "plain"),
+                                       family = "Noto Sans", face = "plain"),
           plot.caption = element_text(margin = margin(t = 10), size = rel(0.6),
-                                      family = "Work Sans Light", face = "plain"),
+                                      family = "Noto Sans", face = "plain"),
           panel.border = element_rect(color = "grey50", fill = NA, size = 0.15),
           panel.spacing = unit(1, "lines"),
           panel.grid.minor = element_blank(),
@@ -71,7 +91,7 @@ theme_donors <- function(base_size = 9, base_family = "Work Sans Light") {
           axis.line = element_blank(),
           axis.ticks = element_blank(),
           axis.title = element_text(size = rel(0.8), 
-                                    family = "Work Sans Medium", face = "plain"),
+                                    family = "Noto Sans", face = "bold"),
           axis.title.y = element_text(margin = margin(r = 10)),
           axis.title.x = element_text(margin = margin(t = 10)),
           legend.position = "bottom",
@@ -80,12 +100,21 @@ theme_donors <- function(base_size = 9, base_family = "Work Sans Light") {
           legend.key = element_blank(),
           legend.spacing = unit(0.1, "lines"),
           strip.text = element_text(size = rel(0.9), hjust = 0,
-                                    family = "Work Sans Medium", face = "plain"),
+                                    family = "Noto Sans", face = "bold"),
           strip.background = element_rect(fill = "white", colour = NA))
-  ret
+  
+  if (prior) {
+    ret <- ret +
+      theme(panel.grid.major = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.y = element_blank(),
+            panel.border = element_blank())
+  } else {
+    ret
+  }
 }
 
-theme_donors_map <- function(base_size = 9, base_family = "Work Sans Light") {
+theme_donors_map <- function(base_size = 9, base_family = "Noto Sans") {
   ret <- theme_void(base_size, base_family) +
     theme(legend.position = "bottom")
   
---
+++ lib/misc_funs.R
@@ -0,0 +1,19 @@
+cumprod_na <- function(x) {
+  x[is.na(x)] <- 1
+  return(cumprod(x))
+}
+
+# Via https://stackoverflow.com/a/55323097/120898
+lhs <- function(x) {
+  if (attr(terms(as.formula(x)), which = "response")) {
+    all.vars(x)[1]
+  } else {
+    NULL
+  }
+}
+
+calc_prob_dist <- function(form, pred, resid, data = df_country_aid_laws) {
+  dnorm(data[[lhs(form$formula)]],
+        pred[,1],
+        sd(resid[,1], na.rm = TRUE))
+}
